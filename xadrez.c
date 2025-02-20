a116ecc765fe87a5d680fdbdebe6c60397ac0426
Pedro Castanho <phh.paulino@gmail.com>
2025-02-20 03:18:01 +0000

    Movimentação do BISPO

--- xadrez.c
+++ xadrez.c
@@ -1,11 +1,19 @@
-        int main() {
-            // Mover a torrre 5 casas para a direita
-        for (int i = 0; i < 5; i++) {
-           printf( "Direita\n");
-        }
-        
-        return 0;
-        }
+#include <stdio.h>
+
+int main() {
+    int passos = 0; // Contador de passos
+
+    while (passos < 6) { // Executa exatamente 5 movimentos
+        printf("%d\n", passos);
+        printf("cima, direita: ");
+        passos++; // Incrementa o contador
+    }
+
+    return 0;
+}
     
     
     
+        //Bispo: 5 casas na diagonal superior direita
+        //Torre: 5 casas para a direita
+        // Rainha: 8 casas para a esquerda
